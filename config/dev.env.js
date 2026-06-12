dd55bfe8e4e8afd327193db2c91d15e5f84a676d
X230976 <shuguang.pan@external.merckgroup.com>
2026-06-12 22:49:01 +0800

    submit yygh-admin code

---
+++ config/dev.env.js
@@ -0,0 +1,9 @@
+'use strict'
+const merge = require('webpack-merge')
+const prodEnv = require('./prod.env')
+
+module.exports = merge(prodEnv, {
+  NODE_ENV: '"development"',
+  // BASE_API: '"http://localhost"',
+  BASE_API: '"http://47.102.86.63:32607"'
+})
---
+++ config/prod.env.js
@@ -0,0 +1,5 @@
+'use strict'
+module.exports = {
+  NODE_ENV: '"production"',
+  BASE_API: '"http://139.198.165.238:32607"',
+}
